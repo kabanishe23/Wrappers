076f5a4f7db39f08cf3f334ad6d696cc015ecacd
Ravil Khusainov <ravil@lattis.io>
2021-03-13 09:26:46 +0300

    Keychain backed key fix

--- Sources/Wrappers/KeychainBacked.swift
+++ Sources/Wrappers/KeychainBacked.swift
@@ -11,19 +11,26 @@ import KeychainSwift
 @propertyWrapper public struct KeychainBacked {
     
     public let key: String
-    private let keychain = KeychainSwift()
+    private let keychain: KeychainSwift
     
-    public init(key: String) {
+    public init(key: String, prefix: String? = nil) {
         self.key = key
+        if let p = prefix {
+            self.keychain = .init(keyPrefix: p)
+        } else {
+            self.keychain = .init()
+        }
     }
     
     public var wrappedValue: String? {
         get {
-            keychain.get(key)
+            return keychain.get(key)
         }
         set {
             if let value = newValue {
-                keychain.set(value, forKey: value)
+                keychain.set(value, forKey: key)
+            } else {
+                keychain.delete(key)
             }
         }
     }
